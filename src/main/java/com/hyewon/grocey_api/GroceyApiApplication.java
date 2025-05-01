e8c70f7da68d1ea0f124edef69f475ded2d15e0c
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-05-01 16:39:02 +0900

    Initialize Spring Boot project

---
+++ src/main/java/com/hyewon/grocey_api/GroceyApiApplication.java
@@ -0,0 +1,13 @@
+package com.hyewon.grocey_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceyApiApplication.class, args);
+	}
+
+}
