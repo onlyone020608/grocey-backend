83742552c638ab0d8194d21963312bb0583acca9
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-05-03 15:48:15 +0900

    feat(user): add User, Allergy, Preference domain entities

---
+++ src/main/java/com/hyewon/grocey_api/domain/user/FoodPreference.java
@@ -0,0 +1,16 @@
+package com.hyewon.grocey_api.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class FoodPreference {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
