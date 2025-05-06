177cec143736141a6db2e723c6976f2c13ce100c
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-05-06 14:55:41 +0900

    refactor(recipe): add constructor for test or init usage

--- src/main/java/com/hyewon/grocey_api/domain/recipe/Recipe.java
+++ src/main/java/com/hyewon/grocey_api/domain/recipe/Recipe.java
@@ -4,10 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Recipe {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
@@ -23,6 +29,10 @@ public class Recipe {
 
     private LocalDateTime savedAt;
 
-
-
+    public Recipe(String recipeName, String description, Integer cookingTime, Integer servings) {
+        this.recipeName = recipeName;
+        this.description = description;
+        this.cookingTime = cookingTime;
+        this.servings = servings;
+    }
 }
