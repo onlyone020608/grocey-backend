b75220166d90171086a68f6bac5e0db911b63e7e
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-05-06 14:43:08 +0900

    feat(recipe): add RecipeIngredient entity and repository

---
+++ src/main/java/com/hyewon/grocey_api/domain/recipe/RecipeIngredientRepository.java
@@ -0,0 +1,9 @@
+package com.hyewon.grocey_api.domain.recipe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
+    List<RecipeIngredient> findByRecipeId(Long recipeId);
+}
