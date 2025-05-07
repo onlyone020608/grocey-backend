45c94e1f335b6c52fe6582dc1961cad8590dd52c
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-05-07 13:40:03 +0900

    chore(init): insert dummy cart and items for testing

--- src/main/java/com/hyewon/grocey_api/init/DataInitializer.java
+++ src/main/java/com/hyewon/grocey_api/init/DataInitializer.java
@@ -1,11 +1,17 @@
 package com.hyewon.grocey_api.init;
 
+import com.hyewon.grocey_api.domain.cart.Cart;
+import com.hyewon.grocey_api.domain.cart.CartItem;
+import com.hyewon.grocey_api.domain.cart.CartItemRepository;
+import com.hyewon.grocey_api.domain.cart.CartRepository;
 import com.hyewon.grocey_api.domain.fridge.Fridge;
 import com.hyewon.grocey_api.domain.fridge.FridgeIngredient;
 import com.hyewon.grocey_api.domain.fridge.FridgeIngredientRepository;
 import com.hyewon.grocey_api.domain.fridge.FridgeRepository;
 import com.hyewon.grocey_api.domain.ingredient.Ingredient;
 import com.hyewon.grocey_api.domain.ingredient.IngredientRepository;
+import com.hyewon.grocey_api.domain.product.Product;
+import com.hyewon.grocey_api.domain.product.ProductRepository;
 import com.hyewon.grocey_api.domain.recipe.Recipe;
 import com.hyewon.grocey_api.domain.recipe.RecipeIngredient;
 import com.hyewon.grocey_api.domain.recipe.RecipeIngredientRepository;
@@ -15,35 +21,47 @@ import com.hyewon.grocey_api.domain.user.Gender;
 import com.hyewon.grocey_api.domain.user.User;
 import com.hyewon.grocey_api.domain.user.UserRepository;
 import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
 @Component
 @RequiredArgsConstructor
-public class DataInitializer {
+public class DataInitializer implements ApplicationRunner {
     private final IngredientRepository ingredientRepository;
     private final FridgeRepository fridgeRepository;
     private final FridgeIngredientRepository fridgeIngredientRepository;
     private final UserRepository userRepository;
     private final RecipeRepository recipeRepository;
     private final RecipeIngredientRepository recipeIngredientRepository;
+    private final ProductRepository productRepository;
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+    @PersistenceContext
+    private EntityManager em;
 
-    @PostConstruct
-    public void initDummyData() {
-        Ingredient i = ingredientRepository.save(new Ingredient("계란", "egg.png"));
+
+    @Override
+    @Transactional
+    public void run(ApplicationArguments args) throws Exception {
         Ingredient egg = ingredientRepository.save(new Ingredient("계란", "egg.png"));
         Ingredient milk = ingredientRepository.save(new Ingredient("우유", "milk.png"));
         Fridge f = fridgeRepository.save(new Fridge(4.0, -18.0));
-
         // userId=1 유저가 있다고 가정하고, 해당 유저에 fridge 연결
-        User u = userRepository.save(new User("TestUser", "onlyone@naver.com", "1234!", AgeGroup.TWENTIES, Gender.FEMALE));
+        User u = new User("TestUser22222", "onlyone@naver.com", "1234!", AgeGroup.TWENTIES, Gender.FEMALE);
+        System.out.println("Fridge User: " + f.getId());
         u.assignFridge(f);
+        userRepository.save(u);
 
         fridgeIngredientRepository.save(
-                new FridgeIngredient(f, i, false, 10, LocalDate.of(2025, 6, 1))
+                new FridgeIngredient(f, egg, false, 10, LocalDate.of(2025, 6, 1))
         );
 
         Recipe recipe = recipeRepository.save(new Recipe(
@@ -55,5 +73,21 @@ public class DataInitializer {
 
         recipeIngredientRepository.save(new RecipeIngredient(recipe, egg, "1개"));
         recipeIngredientRepository.save(new RecipeIngredient(recipe, milk, "100ml"));
+
+        // 장바구니 테스트
+        Product product1 = productRepository.save(new Product("계란 10개입", "롯데", 2980, "egg-pack.png"));
+        Product product2 = productRepository.save(new Product("우유 1L", "롯데", 2450, "milk-1l.png"));
+        Cart cart = new Cart(u, f);
+        cartRepository.save(cart);
+        CartItem item1 = new CartItem(product1, 2);
+        CartItem item2 = new CartItem(product2, 1);
+
+        cart.addCartItem(item1);
+        cart.addCartItem(item2);
+
+        cartItemRepository.save(item1);
+        cartItemRepository.save(item2);
+
+
     }
 }
