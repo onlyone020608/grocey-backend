d0aeb2523eb53b20b01745ad1b8865d274681d3c
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-05-08 16:45:34 +0900

    refactor(recommendation): use User entity instead of userId in recipe recommendation query

--- src/main/java/com/hyewon/grocey_api/domain/recommendation/RecipeRecommendationRepository.java
+++ src/main/java/com/hyewon/grocey_api/domain/recommendation/RecipeRecommendationRepository.java
@@ -1,9 +1,13 @@
 package com.hyewon.grocey_api.domain.recommendation;
 
+import com.hyewon.grocey_api.domain.fridge.Fridge;
+import com.hyewon.grocey_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface RecipeRecommendationRepository extends JpaRepository<RecipeRecommendation, Integer> {
-    List<RecipeRecommendation> findByUserId(Long userId);
+    List<RecipeRecommendation> findByUser(User user);
+
+
 }
--- src/main/java/com/hyewon/grocey_api/domain/recommendation/RecipeRecommendationService.java
+++ src/main/java/com/hyewon/grocey_api/domain/recommendation/RecipeRecommendationService.java
@@ -2,9 +2,12 @@ package com.hyewon.grocey_api.domain.recommendation;
 
 import com.hyewon.grocey_api.domain.cart.dto.CartItemResponseDto;
 import com.hyewon.grocey_api.domain.cart.dto.CartResponseDto;
+import com.hyewon.grocey_api.domain.fridge.Fridge;
+import com.hyewon.grocey_api.domain.fridge.FridgeRepository;
 import com.hyewon.grocey_api.domain.recommendation.dto.RecipeRecommendationDto;
 import com.hyewon.grocey_api.domain.user.User;
 import com.hyewon.grocey_api.domain.user.UserRepository;
+import com.hyewon.grocey_api.global.exception.NotFoundException;
 import com.hyewon.grocey_api.global.exception.RecommendationNotFoundException;
 import com.hyewon.grocey_api.global.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,13 +24,14 @@ public class RecipeRecommendationService {
 
     private final RecipeRecommendationRepository recipeRecommendationRepository;
     private final UserRepository userRepository;
+    private final FridgeRepository fridgeRepository;
 
 
     public List<RecipeRecommendationDto> getRecommendationsByUser(@PathVariable Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserNotFoundException(userId));
 
-        List<RecipeRecommendation> recommendations = recipeRecommendationRepository.findByUserId(userId);
+        List<RecipeRecommendation> recommendations = recipeRecommendationRepository.findByUser(user);
 
         if (recommendations.isEmpty()) {
             throw RecommendationNotFoundException.forUserRecipe(userId);
@@ -37,4 +42,6 @@ public class RecipeRecommendationService {
                 .toList();
 
     }
+
+
 }
