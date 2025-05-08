7b5ce8294d099cfbab53ad39405c683a07bd68b9
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-05-08 15:17:12 +0900

    feat(recommendation): add User FK to RecipeRecommendation entity

--- src/main/java/com/hyewon/grocey_api/domain/recommendation/RecipeRecommendation.java
+++ src/main/java/com/hyewon/grocey_api/domain/recommendation/RecipeRecommendation.java
@@ -2,12 +2,15 @@ package com.hyewon.grocey_api.domain.recommendation;
 
 import com.hyewon.grocey_api.domain.fridge.Fridge;
 import com.hyewon.grocey_api.domain.recipe.Recipe;
+import com.hyewon.grocey_api.domain.user.User;
 import com.hyewon.grocey_api.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class RecipeRecommendation extends BaseTimeEntity {
 
     @Id
@@ -21,6 +24,10 @@ public class RecipeRecommendation extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "recipe_id")
     private Recipe recipe;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @Enumerated(EnumType.STRING)
     private RecommendationType type;
