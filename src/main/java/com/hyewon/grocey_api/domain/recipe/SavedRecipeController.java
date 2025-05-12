a73aba5f74b37d4bafb40443c41144fd160425e1
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-05-13 08:45:44 +0900

    refactor(recipe): secure saved recipe retrieval using @AuthenticationPrincipal

--- src/main/java/com/hyewon/grocey_api/domain/recipe/SavedRecipeController.java
+++ src/main/java/com/hyewon/grocey_api/domain/recipe/SavedRecipeController.java
@@ -2,7 +2,9 @@ package com.hyewon.grocey_api.domain.recipe;
 
 
 import com.hyewon.grocey_api.domain.recipe.dto.SavedRecipeDto;
+import com.hyewon.grocey_api.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +19,8 @@ public class SavedRecipeController {
 
     private final SavedRecipeService savedRecipeService;
 
-    // TODO: Replace {userId} with @AuthenticationPrincipal after implementing JWT
-    @GetMapping("/{userId}/recipes")
-    public List<SavedRecipeDto> getSavedRecipes(@PathVariable Long userId) {
-        return savedRecipeService.getSavedRecipes(userId);
+    @GetMapping("/me/recipes")
+    public List<SavedRecipeDto> getSavedRecipes(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return savedRecipeService.getSavedRecipes(userDetails.getId());
     }
 }
