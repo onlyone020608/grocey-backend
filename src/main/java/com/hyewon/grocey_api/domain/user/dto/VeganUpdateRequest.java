45de8aa9641613291402163c155371f6a1bfe554
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-05-18 15:10:34 +0900

    fix(user-dto): add @JsonProperty for isVegan to fix deserialization issue

--- src/main/java/com/hyewon/grocey_api/domain/user/dto/VeganUpdateRequest.java
+++ src/main/java/com/hyewon/grocey_api/domain/user/dto/VeganUpdateRequest.java
@@ -1,9 +1,11 @@
 package com.hyewon.grocey_api.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class VeganUpdateRequest {
+    @JsonProperty("vegan")
     private boolean isVegan;
 
     public VeganUpdateRequest() {}
