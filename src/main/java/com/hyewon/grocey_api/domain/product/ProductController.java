c7a16a94b48cc1d135f50ca46a9f146e9d2104f9
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-05-19 10:23:55 +0900

    feat(product): implement product detail API

--- src/main/java/com/hyewon/grocey_api/domain/product/ProductController.java
+++ src/main/java/com/hyewon/grocey_api/domain/product/ProductController.java
@@ -3,10 +3,7 @@ package com.hyewon.grocey_api.domain.product;
 
 import com.hyewon.grocey_api.domain.product.dto.ProductDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,10 +13,16 @@ import java.util.List;
 public class ProductController {
 
     private final ProductTabService productTabService;
+    private final ProductService productService;
 
     @GetMapping
     public List<ProductDto> getProductsByTab(@RequestParam(defaultValue = "NEW") String tab) {
         TabType tabType = TabType.valueOf(tab.toUpperCase());
         return productTabService.getProductsByTab(tabType);
+    }
+
+    @GetMapping("/{productId}")
+    public ProductDto getProductDetail(@PathVariable Long productId) {
+        return productService.getProductDetail(productId);
     }
 }
--- src/main/java/com/hyewon/grocey_api/domain/product/ProductService.java
+++ src/main/java/com/hyewon/grocey_api/domain/product/ProductService.java
@@ -1,5 +1,7 @@
 package com.hyewon.grocey_api.domain.product;
 
+import com.hyewon.grocey_api.domain.product.dto.ProductDto;
+import com.hyewon.grocey_api.global.exception.ProductNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +9,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+
+    public ProductDto getProductDetail(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ProductNotFoundException(productId));
+        return new ProductDto(product);
+    }
 }
