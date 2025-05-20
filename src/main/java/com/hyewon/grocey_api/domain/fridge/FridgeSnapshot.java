952b1e2fbd7e038043aed2e104d922ba7d85c84e
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-05-20 17:51:23 +0900

    refactor(fridgeSnapshot): change ingredientId type to Long and add constructor

--- src/main/java/com/hyewon/grocey_api/domain/fridge/FridgeSnapshot.java
+++ src/main/java/com/hyewon/grocey_api/domain/fridge/FridgeSnapshot.java
@@ -2,12 +2,15 @@ package com.hyewon.grocey_api.domain.fridge;
 
 import com.hyewon.grocey_api.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FridgeSnapshot extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
@@ -17,11 +20,18 @@ public class FridgeSnapshot extends BaseTimeEntity {
     @JoinColumn(name = "fridge_id")
     private Fridge fridge;
 
-    private String ingredient_id;
+    private Long ingredient_id;
 
     private Boolean isFreezer;
 
     private int quantity;
+
+    public FridgeSnapshot(Fridge fridge, Long ingredient_id, Boolean isFreezer, int quantity) {
+        this.fridge = fridge;
+        this.ingredient_id = ingredient_id;
+        this.isFreezer = isFreezer;
+        this.quantity = quantity;
+    }
 
 
 
