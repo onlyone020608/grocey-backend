c2186cbc65934deb957d79f23e0fdf047d1c7c08
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-08-05 13:32:04 +0900

    test(recipe-query-service): add unit tests for recipe retrieval

--- src/main/java/com/hyewon/grocey_api/domain/recipe/entity/Recipe.java
+++ src/main/java/com/hyewon/grocey_api/domain/recipe/entity/Recipe.java
@@ -4,15 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
 public class Recipe {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
---
+++ src/test/java/com/hyewon/grocey_api/unit/domain/recipe/RecipeQueryServiceTest.java
@@ -0,0 +1,51 @@
+package com.hyewon.grocey_api.unit.domain.recipe;
+
+import com.hyewon.grocey_api.domain.recipe.entity.Recipe;
+import com.hyewon.grocey_api.domain.recipe.repository.RecipeRepository;
+import com.hyewon.grocey_api.domain.recipe.service.RecipeQueryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class RecipeQueryServiceTest {
+
+    @Mock
+    RecipeRepository recipeRepository;
+    @InjectMocks
+    RecipeQueryService recipeQueryService;
+
+    private Recipe recipe;
+
+    @BeforeEach
+    void setUp() {
+        recipe = Recipe.builder()
+                .id(1L)
+                .build();
+
+    }
+
+    @Test
+    @DisplayName("getRecipes - should return recipes")
+    void getRecipes_shouldSucceed() {
+        // given
+        Long recipeId = 1L;
+        given(recipeRepository.findAllById(List.of(recipeId))).willReturn(
+               List.of(recipe));
+
+        // when
+        List<Recipe> resultRecipes = recipeQueryService.getRecipes(List.of(recipeId));
+
+        // then
+        assertThat(resultRecipes).isEqualTo(List.of(recipe));
+    }
+}
