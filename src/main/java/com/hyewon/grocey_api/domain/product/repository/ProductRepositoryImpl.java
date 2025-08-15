a268e78390e9655df49fe56e0a4b35798820beeb
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-08-16 07:44:51 +0900

    refactor(product): replace implicit join with explicit join and use fetch join to prevent N+1 problem

--- src/main/java/com/hyewon/grocey_api/domain/product/repository/ProductRepositoryImpl.java
+++ src/main/java/com/hyewon/grocey_api/domain/product/repository/ProductRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.hyewon.grocey_api.domain.product.repository;
 
+import com.hyewon.grocey_api.domain.ingredient.entity.QIngredient;
 import com.hyewon.grocey_api.domain.product.entity.QProduct;
 import com.hyewon.grocey_api.domain.product.entity.Product;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -15,12 +16,14 @@ import java.util.stream.Collectors;
 public class ProductRepositoryImpl implements ProductRepositoryCustom {
     private final JPAQueryFactory queryFactory;
     private final QProduct product = QProduct.product;
+    private final QIngredient ingredient = QIngredient.ingredient;
 
     @Override
     public List<Product> findRandomOneEachByIngredient(List<Long> ingredientIds) {
         List<Product> all = queryFactory
                 .selectFrom(product)
-                .where(product.ingredient.id.in(ingredientIds))
+                .join(product.ingredient, ingredient).fetchJoin()
+                .where(ingredient.id.in(ingredientIds))
                 .fetch();
 
         Map<Long, List<Product>> grouped = all.stream()
